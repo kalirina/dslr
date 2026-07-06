d33d3ab8dac5ec1bd85859ba51581e3e7a97335d
Irina Kalinina <irakalina2004@gmail.com>
2026-07-06 15:12:52 +0200

    started prediction

--- src/logreg_predict.py
+++ src/logreg_predict.py
@@ -1,9 +1,23 @@
 import numpy as np
+import json
+import sys
 
 
 def sigmoid(z):
-	return 1 / (1 + np.exp(z))
+    return 1 / (1 + np.exp(z))
 
 
 def main():
-	return sigmoid(theta x's)
+    if sys.args != 3:
+        return
+    try:
+        with open("data/thetas.json", "r") as read_file:
+            thetas = json.load(read_file)
+            return sigmoid(x.dot(thetas))
+    except FileNotFoundError:
+        return 0
+    return sigmoid(theta x's)
+
+
+if __name__ == "__main__":
+    main()
