7a1733b74e328a006161e1c50d57b3287e5bc363
princemyskin <enrmarti@student.42.fr>
2026-07-08 18:14:18 +0200

    flake8

--- src/pair_plot.py
+++ src/pair_plot.py
@@ -2,18 +2,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+
 def main():
     """Displays the pairplot of the whole dataframe to
     help us choose the best features to feed the model"""
     df = pd.read_csv('../datasets/dataset_train.csv')
     df = df.drop('Index', axis='columns')
     numeric_cols = df.select_dtypes(include=['number']).columns.tolist()
-    
+
     columns_to_plot = numeric_cols + ['Hogwarts House']
     plot_df = df[columns_to_plot]
 
     sns.pairplot(plot_df, hue='Hogwarts House', diag_kind='hist')
     plt.show()
+
 
 if __name__ == '__main__':
     try:
