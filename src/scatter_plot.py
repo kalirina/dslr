3d3e0c6c4d638d275717b6397dc1633b6d1462d6
princemyskin <enrmarti@student.42.fr>
2026-07-08 17:18:54 +0200

    flake8

--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+
 def main():
     """We have to answer the question
     'What are the two features that are similar?'
@@ -11,20 +12,20 @@ def main():
     df = pd.read_csv('../datasets/dataset_train.csv')
     df = df.drop('Index', axis='columns')
     numeric_df = df.select_dtypes(include=['number'])
-    
+
     correlation = numeric_df.corr().abs()
-    #Drop the diagonal of 1.0
-    #.triu stands for upper triangle
+    # Drop the diagonal of 1.0
+    # .triu stands for upper triangle
     upper_triangle_mask = np.triu(np.ones(correlation.shape), k=1).astype(bool)
     upper_triangle = correlation.where(upper_triangle_mask)
 
     top_features = upper_triangle.unstack().idxmax()[0:2]
     top_score = upper_triangle.unstack().max()
 
-    print(f"The two most similar features are: {top_features[0]} and {top_features[1]}")
+    print(f"Most similar features: {top_features[0]} and {top_features[1]}")
     print(f"Correlation score: {top_score:.3f}")
 
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     plt.scatter(df[top_features[0]], df[top_features[1]])
 
     plt.xlabel(top_features[0])
@@ -32,6 +33,7 @@ def main():
 
     plt.title('Most similar features')
     plt.show()
+
 
 if __name__ == '__main__':
     try:
